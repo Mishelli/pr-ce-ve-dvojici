912387bf7b79e1b629bb033f59c97a8cfebf5518
unknown <michaela.cekalova@infis.cz>
2025-04-01 08:36:06 +0200

    práce ve dvojici

---
+++ main.py
@@ -0,0 +1,11 @@
+sport1= input ("zadejte první sport")
+sport2 = input ("zadejte druhý sport")
+
+cas_sport1 = int (input("zadejte kolik minut jste dělal první sport"))
+cas_sport2 = int (input ("zadejte kolik minut jste dělal druhý sport"))
+
+výsledek= (cas_sport1 + cas_sport2)
+celkový_výdej= výsledek/5 
+print ("spálil jste" , str(celkový_výdej), "kalorií")
+
+#Míša úkol B
